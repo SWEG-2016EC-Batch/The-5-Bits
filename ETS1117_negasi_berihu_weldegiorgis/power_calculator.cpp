dfb4ed9362420d0fa45dbda42bdc724829074267
nega4 <185200916+nega4@users.noreply.github.com>
2024-12-01 11:46:02 +0300

    power_calculator.cpp

--- ETS1117_negasi_berihu_weldegiorgis/power_calculator.cpp
+++ ETS1117_negasi_berihu_weldegiorgis/power_calculator.cpp
@@ -1,20 +1,28 @@
 /*****************************
-Write a program that find the result of the expression `x`^`y` where the value of X and Y are entered by the user.
+Write a program that finds the result of the expression `x`^`y` 
+where the value of X and Y are entered by the user.
 ***************************************/
 #include <iostream>
 #include <cmath>
 using namespace std;
 
 int main() {
- float x, y, result;
+    float x, y, result;
 
-   cout<<"please enter the base number ";
-   cin>>x;
-   cout<<"please enter the power number  ";
-   cin>>y;
-  
-   result=pow(x,y);
-   cout<<"the value of "<< x <<" to the power of "<<y <<" is "<<result;
- 
- return 0;
+    cout << "Please enter the base number: ";
+    cin >> x;
+    cout << "Please enter the power number: ";
+    cin >> y;
+
+    if ((x == 0 && y == 0) || (x < 0 && y > 0 && y < 1)) {
+        cout << "Invalid input. Please ensure the following:"<<endl;
+        cout << " Both x and y are not zero simultaneously and "
+             << " x < 0 and 0 < y < 1 is not allowed."<<endl;
+    }
+
+    result = pow(x, y);
+
+    cout << "The value of " << x << " to the power of " << y << " is " << result << endl;
+
+    return 0;
 }
