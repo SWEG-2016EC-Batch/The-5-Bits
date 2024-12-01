d06a51f03d3844a583a9da98ce820023e2c01a75
Mohammed Moges <mohammed.moges@aastustudent.edu.et>
2024-12-02 00:18:24 +0300

    Create automobile code.cpp

---
+++ ETS1010-Mohammed-Moges/automobile code.cpp
@@ -0,0 +1,47 @@
+#include <iostream>	
+
+using namespace std;
+
+int main (){
+
+float cap_fuel, mile_perGallon, num_miles;
+
+cout << "Enter the capacity of fuel tank in gallon :\n";
+
+cin >> cap_fuel;
+
+if (cin.fail() || cap_fuel <= 0){
+
+a : cout << "Invalid input!\n";
+
+}
+
+else {
+
+cout << "Enter the miles that an automible can be driven per one gallon :\n";
+
+cin >> mile_perGallon;
+
+if (cin.fail() || mile_perGallon <=0 ){
+ 
+ goto a;
+ 
+}
+
+else {
+
+num_miles = cap_fuel * mile_perGallon; 
+
+
+cout << "The automobile can travel " << num_miles << " miles" <<endl; 
+
+}
+
+}
+
+}
+
+return 0;
+
+}		
+        	
