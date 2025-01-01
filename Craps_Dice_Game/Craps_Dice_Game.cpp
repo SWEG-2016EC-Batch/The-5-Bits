fdba2570326269db77815a628e61316682c1ba97
Nebiyu yalemgeta <nebiyu.yalemgeta@aastustudent.edu.et>
2025-01-01 21:37:50 +0300

    Update Craps_Dice_Game.cpp

--- Craps_Dice_Game/Craps_Dice_Game.cpp
+++ Craps_Dice_Game/Craps_Dice_Game.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 int main() {
-  
+ 
+  int roll1, roll2, sum1;
+        
+        cout<< "\tCraps Dice Game\n";
+
+        rool1 = (rand() % 6) + 1;
+        roll2 = (rand() % 6) + 1;
+        sum1 =  roll1 + roll2;
+        
+        cout  << "You rolled: " << roll1 << " +" <<roll2 << " = " << sum1 << endl;
+
 }
