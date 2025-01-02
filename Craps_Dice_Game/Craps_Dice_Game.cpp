adc51abe20ad504ea1bfe949acc28486f04cd599
nega4 <185200916+nega4@users.noreply.github.com>
2025-01-02 16:57:27 +0300

    Craps_Dice_Game.cpp

--- Craps_Dice_Game/Craps_Dice_Game.cpp
+++ Craps_Dice_Game/Craps_Dice_Game.cpp
@@ -4,15 +4,22 @@ using namespace std;
 
 int main() {
  
-  int roll1, roll2, sum1, sum2, point,won = 0,lost = 0;
-        
+  int roll[2], sum1, sum2, point,won = 0,lost = 0;
         cout<< "\tCraps Dice Game\n";
+
+      string dice[6] = { "┌─────────┐\n│         │\n│    •    │\n│         │\n└─────────┘",
+                         "┌─────────┐\n│•        │\n│         │\n│        •│\n└─────────┘",
+                         "┌─────────┐\n│•        │\n│    •    │\n│        •│\n└─────────┘",
+                         "┌─────────┐\n│•       •│\n│         │\n│•       •│\n└─────────┘",
+                         "┌─────────┐\n│•       •│\n│    •    │\n│•       •│\n└─────────┘",
+                         "┌─────────┐\n│•       •│\n│•       •│\n│•       •│\n└─────────┘"};
          srand(time(0)); 
-        roll1 = (rand() % 6) + 1;
-        roll2 = (rand() % 6) + 1;
-        sum1 =  roll1 + roll2;
-        
-        cout  << "You rolled: " << roll1 << " +" <<roll2 << " = " << sum1 << endl;
+        roll[0] = (rand() % 6) + 1;
+        roll[1] = (rand() % 6) + 1;
+        sum1 =  roll[0] + roll[1];
+         
+        cout << dice[roll[0]-1] << " \n " << dice[roll[1]-1] << endl;
+        cout  << "You rolled: " << roll[0] << " +" <<roll[1] << " = " << sum1 << endl;
  
         if (sum1 == 7 || sum1 == 11) {
             cout << "You win!\n";
@@ -25,10 +32,11 @@ int main() {
             cout << "Your point is: " << point << endl;
         }
  while (sum2 != point && sum2 != 7) {
-                roll1 = (rand() % 6) + 1;
-                roll2 = (rand() % 6) + 1;
-                sum2 = roll1 + roll2; 
-                cout << "You rolled: " << roll1 << " + " << roll2 << " = " << sum2 << endl;
+               roll[0] = (rand() % 6) + 1;
+               roll[1] = (rand() % 6) + 1;
+                sum2 = roll[0] + roll[1]; 
+                cout << dice[roll[0]-1] << " \n " << dice[roll[1]-1] << endl;
+                cout << "You rolled: " << roll[0] << " + " << roll[1] << " = " << sum2 << endl;
         }
             if (sum2 == point) {
                 cout << "You made your point!\nYou win!" << endl;
